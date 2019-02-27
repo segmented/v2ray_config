4d1a67f00d7521a8c86dcc5bb5697f03faaa1eaf
404 <12112909+u403@users.noreply.github.com>
2019-02-27 13:09:17 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,17 +5,20 @@
 #install v2ray
 
 curl https://install.direct/go.sh | sh
-
-yum -y install lrzsz socat
-
-#make cert
-curl https://get.acme.sh | sh
 source ~/.bashrc
 
-~/.acme.sh/acme.sh --issue -d de.auxssrost.cf --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d de.auxssrost.cf --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
+#yum -y install lrzsz socat
+#make cert
+#curl https://get.acme.sh | sh
+#source ~/.bashrc
+#~/.acme.sh/acme.sh --issue -d de.auxssrost.cf --standalone -k ec-256
+#~/.acme.sh/acme.sh --installcert -d de.auxssrost.cf --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
 
-rm -f /etc/v2ray/config.json
-wget 
-#v2ray start
+systemctl stop firewalld.service
+cd /etc/v2ray
+rm -f config.json
+wget https://raw.githubusercontent.com/u403/v2ray_config/master/config.json
+wget https://raw.githubusercontent.com/u403/v2ray_config/master/v2ray.crt
+wget https://raw.githubusercontent.com/u403/v2ray_config/master/v2ray.key
+
 systemctl start v2ray
