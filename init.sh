936848976f5a7b8d209c788da577c1b36351eaa9
404 <12112909+segmented@users.noreply.github.com>
2022-01-14 15:25:59 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@
 #install v2ray
 
 #curl https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh | sh
-curl https://raw.githubusercontent.com/segmented/fhs-install-v2ray/master/install-release.sh --version=4.43.0 | bash
+curl https://raw.githubusercontent.com/segmented/fhs-install-v2ray/master/install-release.sh | bash
 #curl https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh | sh
 curl https://raw.githubusercontent.com/segmented/fhs-install-v2ray/master/install-dat-release.sh | bash
 source ~/.bashrc
