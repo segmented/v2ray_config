8fff182e3fdc9d255473956f4e9322380d0c66ae
404 <12112909+segmented@users.noreply.github.com>
2022-09-29 23:24:44 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -21,9 +21,9 @@ source ~/.bashrc
 #~/.acme.sh/acme.sh --installcert -d de.auxssrost.cf --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
 
 systemctl stop firewalld.service
-cd /etc/systemd/system/v2ray.service.d/
-rm -f 10-donot_touch_single_conf.conf
-wget https://raw.githubusercontent.com/segmented/v2ray_config/master/10-donot_touch_single_conf.conf
+#cd /etc/systemd/system/v2ray.service.d/
+#rm -f 10-donot_touch_single_conf.conf
+#wget https://raw.githubusercontent.com/segmented/v2ray_config/master/10-donot_touch_single_conf.conf
 
 cd /usr/local/etc/v2ray/
 rm -f config.json
@@ -36,8 +36,10 @@ wget https://raw.githubusercontent.com/segmented/v2ray_config/master/config.json
 #rm -f config.json
 #wget https://raw.githubusercontent.com/segmented/v2ray_config/master/config.json
 
+rm -rf /etc/systemd/system/v2ray.service.d/10-donot_touch_single_conf.conf
 systemctl daemon-reload
-#systemctl restart v2ray
+systemctl restart v2ray
+
 #systemctl start v2ray
-#v2ray run
+v2ray run
 #v2ray 
