17a8bf690301cb4343a0673e9e91c020808d46ee
404 <12112909+segmented@users.noreply.github.com>
2022-09-30 11:36:56 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 #yum -y update
 
-#install v2ray
-
-#curl https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh | sh
-curl https://raw.githubusercontent.com/segmented/fhs-install-v2ray/master/install-release.sh |  bash
-
 #back to 4.45.0
 #wget https://raw.githubusercontent.com/segmented/fhs-install-v2ray/master/install-release.sh
 #bash  install-release.sh  --version v4.45.0
-#curl https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh | sh
+
+curl https://raw.githubusercontent.com/segmented/fhs-install-v2ray/master/install-release.sh |  bash
 curl https://raw.githubusercontent.com/segmented/fhs-install-v2ray/master/install-dat-release.sh | bash
 source ~/.bashrc
 
-#yum -y install lrzsz socat
 #make cert
 #curl https://get.acme.sh | sh
 #source ~/.bashrc
@@ -21,9 +16,6 @@ source ~/.bashrc
 #~/.acme.sh/acme.sh --installcert -d de.auxssrost.cf --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
 
 systemctl stop firewalld.service
-#cd /etc/systemd/system/v2ray.service.d/
-#rm -f 10-donot_touch_single_conf.conf
-#wget https://raw.githubusercontent.com/segmented/v2ray_config/master/10-donot_touch_single_conf.conf
 
 cd /usr/local/etc/v2ray/
 rm -f config.json
@@ -31,15 +23,5 @@ wget https://raw.githubusercontent.com/segmented/v2ray_config/master/certs/v2ray
 wget https://raw.githubusercontent.com/segmented/v2ray_config/master/certs/v2ray.key
 wget https://raw.githubusercontent.com/segmented/v2ray_config/master/cfgs/config.json
 
-#mkdir /usr/local/etc/v2ray
-#cd /usr/local/etc/v2ray
-#rm -f config.json
-#wget https://raw.githubusercontent.com/segmented/v2ray_config/master/cfgs/config.json
-
-rm -rf /etc/systemd/system/v2ray.service.d/10-donot_touch_single_conf.conf
 systemctl daemon-reload
 systemctl restart v2ray
-
-#systemctl start v2ray
-#v2ray run
-#v2ray 
